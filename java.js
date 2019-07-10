b1eafb09d3e2afffb2b77db61419a381d0b57127
unknown <elizabeth.falcon@cristoreysj.org>
2019-07-10 09:36:48 -0700

    this is my first commit!

---
+++ java.js
@@ -0,0 +1,27 @@
+function alert_name() {
+  //Create name variable
+  var name = "Elizabeth";
+  alert("My name is " + name)
+console.log("Test 123")
+}
+
+function change_image() {
+  var image = document.getElementById('Gerald-image')
+  image.src = "img/gerald.jfif"
+}
+
+
+function change_image() {
+  var image = document.getElementById('Gerald-image')
+  image.src = "img/images.jfif"
+}
+
+function increase_size() {
+  var image = document.getElementById('bunny_image');
+  image.style.width = "800px";
+}
+
+funtion decrease_size() {
+  var image = document.getElementById('image')
+  image.style.width = "400px";
+}
